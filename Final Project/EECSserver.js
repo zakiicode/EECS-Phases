fd60fa12ac5e7d896b5e47fc9b942319ddbc77be
zakiicode <92188960+zakiicode@users.noreply.github.com>
2021-12-04 17:08:48 -0500

    Update EECSserver.js

--- Final Project/EECSserver.js
+++ Final Project/EECSserver.js
@@ -1,8 +1,6 @@
-var codes = {}; // an empty JS object, later it's going to store the code for each end-user
-
 var express = require('express');
 var app = express();
-var idCounter = 0;
+var round = Math.floor(Math.random()*3+1);
 
 app.post('/post', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
@@ -12,12 +10,11 @@ app.post('/post', (req, res) => {
     var z = JSON.parse(req.query['data']);
     
     if (z['action'] == "evaluatesing") {
-        /* the response will have 6 parts: request action, whether won or not, number of exact matches,
-        number of colors that are correct, number of wrong colors, and the answer if the game ended */
         var p1= z['choice'];
         var win = false;
         var pc = Math.floor(Math.random()*3+1);
-
+        var diff = z['diff'];
+        if(diff == 1){
         // 1 is rock, 2 is paper, 3 is scissors
         if ((p1 == 1 && pc == 2)||(p1 == 2 && pc == 3)||(p1 == 3 && pc == 1)){
             win = false;
@@ -26,13 +23,21 @@ app.post('/post', (req, res) => {
             win = true;
         }
         else if((pc ==p1)){
-        // console.log("tie");
            win="tie";
         }
+    }
+    else if(diff == 2){
+        if(round == Math.floor(Math.random()*3+1)){
+            win = true;
+        }
+        else{
+            win = false;
+        }
+    }
         var jsontext = JSON.stringify({
             'name':z['name'],
             'p1win':win,
-            /*TODO 3 ... type of action */
+            'diff':diff,
             'action':'evaluatesing'
         });
         console.log(jsontext);
@@ -56,7 +61,7 @@ app.post('/post', (req, res) => {
             'name':z['name'],
             'name2':z['name2'],
             'p1win':win,
-            /*TODO 3 ... type of action */
+            'diff':diff,
             'action':'evaluate'
         });
         console.log(jsontext);
@@ -67,45 +72,3 @@ app.post('/post', (req, res) => {
     }
 }).listen(3000);
 console.log("Server is running!");
-
-/*
- * Evaluate the client's attempting code
- * @param code is the server generated code for this client
- * @param attempt_code is the client attempted code in this request
- * @return num_match, num_containing, num_not_in
-
-function evaluate(p1, p2) {
-    var p1win =false;
-    var pc = Math.floor(Math.random()*3+1);
-    //console.log(pc);
-    if(p2==0){
-        // 1 is rock, 2 is paper, 3 is scissors
-        if ((p1 == 1 && pc == 2)||(p1 == 2 && pc == 3)||(p1 == 3 && pc == 1)){
-            p1win = false;
-            }
-        else if((pc == 1 && p1 == 2)||(pc == 2 && p1 == 3)||(pc == 3 && p1 == 1)){
-            p1win = true;
-        }
-        else if((pc ==p1)){
-        // console.log("tie");
-           p1win="tie";
-        }
-        return p1win;
- }
- else{
-        // 1 is rock, 2 is paper, 3 is scissors
-    if ((p1 == 1 && p2 == 2)||(p1 == 2 && p2 == 3)||(p1 == 3 && p2 == 1) && PCSCORE<3){
-        
-        p1win = false;
-    }
-    else if((p2 == 1 && p1 == 2)||(p2 == 2 && p1 == 3)||(p2 == 3 && p1 == 1)&&P1SCORE<3){
-        p1win = true;
-    }
-    else if((p2 == p1) && P1SCORE<3&&PCSCORE<3){
-        p1win="tie";
-    }
-        return p1win;
-
- }
-  
-} */
